5b215868f3c4b1b8afe82a40969df86cbb61b670
siva2245 <mailtosiva2245@gmail.com>
2023-11-10 17:16:54 +0530

    updated

--- Test/Test/src/Programs/Program1.java
+++ Test/Test/src/Programs/Program1.java
@@ -11,16 +11,6 @@ public class Program1 {
 
         
 
-        for (int i = 0; i< n; i++) {
-            for (int sp = 0; sp < i; sp++) {
-                System.out.print("  ");
-            }
-
-            for (int j = 0; j < n; j++) {
-                if (arr[i][j] != 0)
-                    System.out.print(arr[i][j] + " ");
-            }
-            System.out.println();
-        }
+        
     }
 }
