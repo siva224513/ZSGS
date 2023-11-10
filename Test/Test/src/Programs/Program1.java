a6d07feb2719811445d225d310cab4d0641d8cff
siva2245 <mailtosiva2245@gmail.com>
2023-11-10 17:43:49 +0530

    updated

--- Test/Test/src/Programs/Program1.java
+++ Test/Test/src/Programs/Program1.java
@@ -1,16 +1,49 @@
 package Programs;
 
 import java.util.Scanner;
-
 public class Program1 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        Scanner scan=new Scanner(System.in);
+        System.out.println("enter the number");
+        int n=scan.nextInt();
+      
         int[][] arr = new int[n][n];
-        int k = 1;
+        int value=1;  //count
+        int left=0,right=n-1;
+        int top=0,down=n-1;
+        int ld= (int) Math.pow(n,2);
+        for(int i=n-1;i>0;i--){
+            ld = ld -i;
+        }
+        while(value<=ld){
+            for(int i=left;i<=right;i++){
+                arr[i][top]=value;
+                value++;
+            }
+            top++;right--;
+            for(int i=right;i>=left;i--){
+                arr[i][down]=value;
+                value++;
+            }
+            down--;
+            for(int i=down;i>=top;i--){
+                arr[left][i]=value;
+                value++;
+            }
+            left++;right--;
+        }
 
-        
+        for(int i=0;i<n;i++){
+            for(int space=0;space<i;space++){
+                System.out.print("  ");
+            }
 
-        
+            for(int j=0;j<n;j++){
+                if(arr[i][j]==0)
+                    continue;
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
     }
 }
