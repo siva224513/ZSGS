1e93297ad8e0e2a7d79e59da52cc87b7ffbb71db
siva2245 <mailtosiva2245@gmail.com>
2023-11-18 08:30:12 +0530

    first

--- Test/Test/src/Programs/Program1.java
+++ Test/Test/src/Programs/Program1.java
@@ -11,43 +11,8 @@ public class Program1 {
         int value=1;  //count
         int left=0,right=n-1;
         int top=0,down=n-1;
-        int ld= (int) Math.pow(n,2);
-        for(int i=n-1;i>0;i--){
-            ld = ld -i;
-        }
-        while(value<=ld){
-            for(int i=left;i<=right;i++){
-                arr[i][top]=value;
-                value++;
-            }
-            top++;right--;
-            for(int i=right;i>=left;i--){
-                arr[i][down]=value;
-                value++;
-            }
-            down--;
-            for(int i=down;i>=top;i--){
-                arr[left][i]=value;
-                value++;
-            }
-            left++;right--;
-        }
-
-        for(int i=0;i<n;i++){
-            for(int space=0;space<i;space++){
-                System.out.print("  ");
-            }
-
-            for(int j=0;j<n;j++){
-                if(arr[i][j]==0)
-                    continue;
-                System.out.print(arr[i][j]+" ");
-            }
-            System.out.println();
-        }
-    }
-   /*Adding this solution on 11/11/23 - saturday
-
+        
+       
        while(top<=down && left<=right){
           for(int i=top;i<=down;i++){
               arr[i][left+i]=value++;
@@ -66,7 +31,7 @@ public class Program1 {
           }
          top++;
          down--;
-
+        }
 
         for (int i = 0; i < arr.length; i++) {
          for (int j = 0; j < arr[i].length; j++) {
@@ -79,6 +44,9 @@ public class Program1 {
            System.out.println();
          }
     }
-    * 
-    */
+
+        
+        
+    
+  
 }
