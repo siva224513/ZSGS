0e175c3feafee21bbfde3b2ff55f603fe0454505
siva224513 <mailtosiva2245@gmail.com>
2023-12-23 11:55:56 +0530

    level2update

--- Console Application/dungeongame/src/com/zsgs/dungeongame/DungeonGame.java
+++ Console Application/dungeongame/src/com/zsgs/dungeongame/DungeonGame.java
@@ -4,29 +4,47 @@ import java.util.Scanner;
 
 public class DungeonGame {
     public static Scanner scanner = new Scanner(System.in);
+    public static int row = 0, column = 0, adventurerPositionColumn = 0, adventurerPositionRow = 0, goldPostionRow = 0,
+            goldPostionColumn = 0, monsterPositionRow = 0, monsterPositionColumn = 0;
 
     public static void main(String[] args) throws Exception {
         Levels levels = new Levels();
 
         System.out.println("Welcome to DungeonGame");
-        System.out.println("1.Adventurer and Gold\n");
+        System.out.println("1.Adventurer and Gold\n2.Adventurer and gold with monster");
 
         int level = scanner.nextInt();
         switch (level) {
             case 1:
                 System.out.println("Dimensions of the dungeon(Row X column):");
-                int row = scanner.nextInt();
-                int column = scanner.nextInt();
-                int box[][] = new int[row][column];
+                row = scanner.nextInt();
+                column = scanner.nextInt();
                 System.out.println("Enter Position(coordinates) of the adventurer");
-                int adventurerPositionRow = scanner.nextInt();
-                int adventurerPositionColumn = scanner.nextInt();
+                adventurerPositionRow = scanner.nextInt();
+                adventurerPositionColumn = scanner.nextInt();
                 System.out.println("Enter position(coordinates) of the gold");
-                int goldPostionRow = scanner.nextInt();
-                int goldPostionColumn = scanner.nextInt();
-                levels.level1(box, adventurerPositionRow-1, adventurerPositionColumn-1, goldPostionRow-1, goldPostionColumn-1);
+                goldPostionRow = scanner.nextInt();
+                goldPostionColumn = scanner.nextInt();
+                levels.level1(row, column, adventurerPositionRow - 1, adventurerPositionColumn - 1, goldPostionRow - 1,
+                        goldPostionColumn - 1);
                 break;
+            case 2:
+                System.out.println("Dimensions of the dungeon(Row X column):");
+                row = scanner.nextInt();
+                column = scanner.nextInt();
+                System.out.println("Enter Position(coordinates) of the adventurer");
+                adventurerPositionRow = scanner.nextInt();
+                adventurerPositionColumn = scanner.nextInt();
+                System.out.println("Enter position of the monster");
+                monsterPositionRow = scanner.nextInt();
+                monsterPositionColumn = scanner.nextInt();
+                System.out.println("Enter position(coordinates) of the gold");
+                goldPostionRow = scanner.nextInt();
+                goldPostionColumn = scanner.nextInt();
 
+                levels.level2(row, column, adventurerPositionRow - 1, adventurerPositionColumn - 1, goldPostionRow - 1,
+                        goldPostionColumn - 1, monsterPositionRow - 1, monsterPositionColumn - 1);
+                break;
             default:
                 break;
         }
--- Console Application/dungeongame/src/com/zsgs/dungeongame/Levels.java
+++ Console Application/dungeongame/src/com/zsgs/dungeongame/Levels.java
@@ -3,17 +3,32 @@ package com.zsgs.dungeongame;
 import java.util.Arrays;
 
 public class Levels {
-    public void level1(int box[][], int adventurerPositionRow, int adventurerPositionColumn, int goldPostionRow,
+    public void level1(int row, int column, int adventurerPositionRow, int adventurerPositionColumn, int goldPostionRow,
             int goldPostionColumn) {
 
         // for (int b[] : box) {
-        //     Arrays.fill(b, 1);
+        // Arrays.fill(b, 1);
         // }
         // box[adventurerPositionRow][adventurerPositionColumn]=0;
         // box[goldPostionRow][goldPostionColumn]=100;
         // System.out.println(Arrays.deepToString(box));
         System.out.println("Minimum Number of Steps");
-        System.out.println(Math.abs(adventurerPositionRow-goldPostionRow)+Math.abs(adventurerPositionColumn-goldPostionColumn));
+        System.out.println(Math.abs(adventurerPositionRow - goldPostionRow)
+                + Math.abs(adventurerPositionColumn - goldPostionColumn));
 
+    }
+
+    public void level2(int row, int column, int adventurerPositionRow, int adventurerPositionColumn, int goldPostionRow,
+            int goldPostionColumn, int monsterPositionRow, int monsterPositionColumn) {
+
+        int monsterMinPath = Math.abs(monsterPositionRow - goldPostionRow)
+                + Math.abs(monsterPositionColumn - goldPostionColumn);
+        int adventurerMinPath = Math.abs(adventurerPositionRow - goldPostionRow)
+                + Math.abs(adventurerPositionColumn - goldPostionColumn);
+        if (adventurerMinPath <= monsterMinPath) {
+            System.out.println("Minimum number of steps: " + adventurerMinPath);
+        } else {
+            System.out.println("No possible solution");
+        }
     }
 }
